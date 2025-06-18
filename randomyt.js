27663c14b58482ec2acb0735d6eeb0a71a379152
Trịnh Viễn <tnvien2009@gmail.com>
2025-06-18 22:00:04 +0700

    Update randomyt.js

--- randomyt.js
+++ randomyt.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const videoLinks = [
-    "https://youtu.be/pCbaNYGsqOs?si=DKGsHV2bDlQxtkOh",
-    "https://youtu.be/ZGhTjouWqoQ?si=ibg07HrnYHHTYtmE",
-    "https://youtu.be/jDu2103LJIo?si=g0Ua5xeTIik6h3Xl",
-    "https://youtu.be/UUVfum1EpYY?si=yTXf3Je2OmjWqxfc",
-    "https://youtu.be/vVHHrX91Qjw?si=UNkoTK8-HJ9kfJm-",
-    "https://youtu.be/MpIAp0n_giM?si=HeXDNn6LQaSNJeYI",
-    "https://youtu.be/5RNNtw-DyQs?si=mfDavIMAhy7hJrKx",
-    "https://youtu.be/ZxPxDjmTpyU?si=4aj-Wng3ouPqNYXp",
-    "https://youtu.be/AXzEeFkmev8?si=gy93GJUkZCCNSVm6",
-    "https://youtu.be/AGF-QP0rTnc?si=tR_mAFEYlSJIhOP-"
+    "https://www.youtube.com/embed/pCbaNYGsqOs?autoplay=1&mute=1",
+    "https://www.youtube.com/embed/ZGhTjouWqoQ?autoplay=1&mute=1",
+    "https://www.youtube.com/embed/jDu2103LJIo?autoplay=1&mute=1",
+    "https://www.youtube.com/embed/UUVfum1EpYY?autoplay=1&mute=1",
+    "https://www.youtube.com/embed/vVHHrX91Qjw?autoplay=1&mute=1",
+    "https://www.youtube.com/embed/MpIAp0n_giM?autoplay=1&mute=1",
+    "https://www.youtube.com/embed/5RNNtw-DyQs?autoplay=1&mute=1",
+    "https://www.youtube.com/embed/ZxPxDjmTpyU?autoplay=1&mute=1",
+    "https://www.youtube.com/embed/AXzEeFkmev8?autoplay=1&mute=1",
+    "https://www.youtube.com/embed/AGF-QP0rTnc?autoplay=1&mute=1"
   ];
 
   const ytFrame = document.getElementById("ytFrame");
